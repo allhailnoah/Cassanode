a9c354f2f8eaf2611cf58a92efbc87f7e5d3b7e9
Michał Sidor <michcioperz@autistici.org>
2013-12-03 18:59:34 +0100

    My first script in NodeJS. Wow

---
+++ bot.js
@@ -0,0 +1,21 @@
+var irc = require("irc");
+var bot = new irc.Client("irc.afternet.org", "Cassandra", {channels:["#GamedevTeam","#ScratchTheCat","#AHN"]});
+bot.addListener("join", function(channel, who) {
+    bot.notice(channel, irc.colors.wrap(irc.colors.codes.dark_red,who.toUpperCase() + " ARRIVES IN A BLAZE OF GLORY"));
+});
+bot.addListener("message", function(nick, to, text, message) {
+    if (message.search("o/") != -1) {
+        bot.message(to, "\o");
+    }
+    if (message.search("\o") != -1) {
+        bot.message(to, "o/");
+    }
+    if (message.search("!Bosozoku") != -1) {
+        bot.whois(nick, function(info) {
+            if (info.host.search("michcioperz.Users.Afternet.Org") != -1) {
+                bot.disconnect();
+                process.exit();
+            }
+        });
+    }
+});
