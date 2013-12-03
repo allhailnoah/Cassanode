33937cbab3d64f38eda5a7ab49ffa16fc3395aca
Michał Sidor <michcioperz@autistici.org>
2013-12-03 21:50:12 +0100

    I think I readded that funny thing with ears

--- bot.js
+++ bot.js
@@ -10,9 +10,15 @@ bot.addListener("message", function(from, to, message) {
     if (message.indexOf("\\o") > -1) {
         bot.say(to, "o/");
     }
+    if (message.indexOf("Cassandra, can you hear me?") > -1) {
+        bot.say(to, "Yes.");
+    }
+    if (message.indexOf("Are you ready to begin?") > -1) {
+        bot.say(to, "Yes, I'm ready.");
+    }
     if (message.indexOf("!Bosozoku") > -1) {
         bot.whois(from, function(info) {
-            if (info.host.indexOf("michcioperz.Users.Afternet.Org") > -1) {
+            if (info.host.indexOf("michcioperz.Users.AfterNET.Org") > -1) {
                 bot.disconnect();
                 process.exit();
             }
